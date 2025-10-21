7d39ba5e436751fa3ebd95c206512648231a109d
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 10:13:00 +0700

    Add files via upload

---
+++ PRAKTIK 6.8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRAKTIK_6._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ulangPilihan;
+
+            do
+            {
+
+                // --- Blok proses yang akan diulang ---
+                Console.WriteLine("-----------------------------------");
+                Console.Write("Masukkan nama anda: ");
+                string nama = Console.ReadLine();
+                Console.WriteLine($"Hallo, {nama}! Proses telah selesai. ");
+                //------------------------------
+                // pertanyaan untuk mengulang
+                Console.Write("Apakah anda ingin mengulang lagi? (iya/tidak): ");
+                ulangPilihan = Console.ReadLine().ToLower(); // mengambil input dan mengubah ke huruf kecil
+
+                Console.WriteLine(); // baris kosong untuk kerapian
+
+                // kondisi diperiksa di akhir: ulangi selama pengguna mengetik "iya"
+            } while (ulangPilihan == "ya");
+            Console.WriteLine("Terima kasih. program selesai.");
+        }
+    }
+}
