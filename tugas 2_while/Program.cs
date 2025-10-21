ac5301b47b1c0f9330511d5458a9658c42de1630
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 10:16:52 +0700

    Add files via upload

---
+++ tugas 2_while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas_2_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int total = 0;
+
+            while (i <= 5)
+            {
+                total += i;
+                i++;
+            }
+            Console.WriteLine("total = " + total);
+        }   
+    }
+}
