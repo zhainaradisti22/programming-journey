48c79ee910a8287e185fe567d26b0f4dc3aa4788
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 10:10:32 +0700

    Add files via upload

---
+++ praktik 6.5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._5_yulmenina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Inisialisasi variabel penghitung (counter)
+            int angka = 1;
+
+            Console.WriteLine("Menampilkan angka 1 sampai 20: ");
+
+            // 2. Blok 'do-while'
+            do
+            {
+                // Tampilkan nilai variabel 'angka' saat ini 
+                Console.WriteLine(angka);
+
+                // Tambahkan nilai 'angka' sebanyak 1 (increment)
+                angka++;
+
+                // Uji kondisi: Loop berlanjut selama 'angka' kurang dari atau sama dengan 
+            } while (angka <= 20);
+
+            Console.WriteLine("Selesai");
+        }
+    }
+}
