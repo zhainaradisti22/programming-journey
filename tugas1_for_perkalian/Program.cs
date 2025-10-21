3966353c6f67aa9431f86b93e980849d0ab1fba5
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 10:14:40 +0700

    Add files via upload

---
+++ tugas1_for_perkalian/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tugas1_for_perkalian
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int hasil = 5 * i;
+                Console.WriteLine("5 * " + i + " = " + hasil);
+            }
+            
+        }
+    }
+}
