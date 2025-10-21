dcaa2185d114913d3bacef0db6ff6b5aa40d0f3f
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 15:03:50 +0700

    Add files via upload

---
+++ fungsi_hitung_luas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fungsi_hitung_luas
+{
+    internal class Program
+    {
+        static double HitungLuas(double panjang, double lebar)
+        {
+            double luas = panjang * lebar;
+            return luas;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan panjang: ");
+            double p = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Masukkan lebar: ");
+            double l = Convert.ToDouble(Console.ReadLine());
+
+            double hasil = HitungLuas(p, l);
+            Console.WriteLine("Luas persegi panjang adalah: " + hasil);
+        }
+
+    }
+}
