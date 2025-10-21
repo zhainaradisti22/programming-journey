6f287851d391e9f36869c59f9c180b712dfdd5bd
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 10:05:25 +0700

    Add files via upload

---
+++ praktik 6.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._2_yulmenina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variabel untuk menyimpan hasil penjumlahan 
+            int total = 0;
+
+            Console.WriteLine("Menghitung jumlah angka dari 1 sampai 5:");
+
+            // Perulangan for dari 1 hingga 5
+            for (int i = 1; i <= 5; i++)
+            {
+                // Tambahkan nilai i saat ini ke variabel total
+                total = total + i; // Bisa juga ditulis sebagai: total += i;
+
+                // Tampilkan prosses penjumlahan di setiap langkah 
+                Console.WriteLine("Menambahkan " + i + ", Total sementara = " + total);
+            }
+            // Tampilkan hasil akhir setelah pengulangan selesai
+            Console.WriteLine("\nHasil akhir penjumlahan adalah : " + total);
+        }
+    }
+}
