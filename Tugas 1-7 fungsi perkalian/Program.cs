1e33b4e13bc6bfd98b28682eb4c67a0e3fef6a01
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 15:05:08 +0700

    Add files via upload

---
+++ Tugas 1-7 fungsi perkalian/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_1_7_fungsi_perkalian
+{
+    internal class Program
+    {
+        static int Kali(int a, int b, int c)
+        {
+            return a * b * c;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka pertama: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan angka kedua: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan angka ketiga: ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int hasil = Kali(a, b, c);
+            Console.WriteLine("Hasil perkalian: " + hasil);
+        }
+    }
+}
