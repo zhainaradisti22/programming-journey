0184a524e5ddf4874726a37cbcc45644f369e24a
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 15:01:12 +0700

    Add files via upload

---
+++ praktik_7.1_fungsi/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_7._1_fungsi
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+            Console.WriteLine("Halo Selamat Datang di pembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi");
+        }
+        static void Main(string[] args)
+        {
+            Sapa(); // Memanggil fungsi
+        }
+    }
+}
