839c7f60f3fbfe866f8f16ba554c775674e7184c
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 10:06:36 +0700

    Add files via upload

---
+++ praktik 6.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktik_6._3_yulmenina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inisialisasi variabel untuk menyimpan input
+            string inputUser = "";
+
+            Console.WriteLine("Program akan terus berjalan hingga anda mengetik 'keluar'");
+
+            // Perulangan selama isi variabel inputUser TIDAK SAMA DENGAN "keluar"
+            while (inputUser.ToLower() != "keluar") ;
+            {
+                Console.Write("\nketik ssesuatu (atau 'keluar' untuk berhenti): ");
+                inputUser = Console.ReadLine(); // Baca input dari User
+
+                Console.WriteLine("Anda mengetik : " + inputUser);
+            }
+            Console.WriteLine("\nProgram selesai. terima kasih!");
+        }
+    }
+}
