400f7ec36ebb028239c07fe6668e8114a3cd8883
zhainaradisti22 <zhaina258778@student.smkn1kandeman.sch.id>
2025-10-21 15:05:43 +0700

    Add files via upload

---
+++ Tugas 2-7 fungsi rata-rata/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_2_7_fungsi_rata_rata
+{
+    internal class Program
+    {
+        // fungsi RataRata : menerima tiga nilai dan mengembalikan hasil rata ratanya
+        static double RataRata(double a, double b, double c)
+        {
+            double hasil = (a + b + c) / 3;
+            return hasil;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Program Menghitung Rata-Rata ===");
+
+            // Meminta input dari pengguna
+            Console.Write("Masukkan nilai pertama : ");
+            double nilai1 = double.Parse(Console.ReadLine());
+            Console.Write("Masukkan nilai kedua : ");
+            double nilai2 = double.Parse(Console.ReadLine());
+            Console.Write("Masukkan nilai ketiga : ");
+            double nilai3 = double.Parse(Console.ReadLine());
+
+            // Memanggil fungsi Rata-Rata dan menampilkan hasilnya
+            double hasilRata = RataRata(nilai1, nilai2, nilai3);
+            Console.WriteLine("Hasil rata-rata : " +  hasilRata);
+        }
+    }
+}
---
+++ Tugas 2-7 fungsi rata-rata/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Tugas 2-7 fungsi rata-rata")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tugas 2-7 fungsi rata-rata")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("31af2a87-bf31-4ca0-b726-c26c0196d9d5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
